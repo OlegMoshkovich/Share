2889cfb06e61bd854ebb6ddc70a017282fafda80
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-13 13:01:18 +0100

    change the application error and adjust colors

--- src/Components/ApplicationError.jsx
+++ src/Components/ApplicationError.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import {grey} from '@mui/material/colors'
+import useTheme from '@mui/styles/useTheme'
 import Logo from '../assets/LogoB.svg'
 
 
 const ApplicationError = () => {
+  const theme = useTheme()
   return (
     <Box p={4} mx={'auto'}
       sx={{
         width: 400,
-        bgcolor: grey[100],
+        bgcolor: theme.palette.scene.background,
         borderRadius: '10px',
       }}
     >
-      <Typography variant={'body1'} sx={{color: 'primary.light', fontWeight: 600, pb: 2}}>
+      <Typography variant={'body1'} sx={{color: theme.palette.primary.main, fontWeight: 600}}>
         Oh no!
       </Typography>
 
-      <Typography variant={'body1'} sx={{color: 'success.dark', fontWeight: 1000, pb: 2}}>
+      <Typography variant={'body1'} sx={{color: theme.palette.secondary.main, fontWeight: 1000}}>
         We&apos;re not quite sure what went wrong.
       </Typography>
 
-      <Typography sx={{pb: 2, lineHeight: 1.75}}>
+      <Typography variant={'body1'} sx={{pb: 2, color: theme.palette.secondary.main}}>
         Not to worry.<br/>
         You can <a href="/">click here to start a new session</a>.
       </Typography>
